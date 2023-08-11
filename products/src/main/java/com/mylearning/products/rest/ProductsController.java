ebde4f36e6d28ad93a30ea807c4f53f8c05f168a
Bladimir Gonzales <dart5045@gmail.com>
2023-08-11 19:24:04 +0200

    Add CQRS Pattern
    Add CQRS pattern to products microservice

--- products/src/main/java/com/mylearning/products/rest/ProductsController.java
+++ products/src/main/java/com/mylearning/products/rest/ProductsController.java
@@ -1,20 +1,48 @@
 package com.mylearning.products.rest;
 
+import com.mylearning.products.rest.command.CreateProductCommand;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
 
 public class ProductsController {
 
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
+    public ProductsController(Environment env, CommandGateway commandGateway) {
+        this.env = env;
+        this.commandGateway = commandGateway;
+    }
+
     @PostMapping
-    public String createProduct(){
-        return "Post";
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel){
+
+        CreateProductCommand productCommand = CreateProductCommand
+                .builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString())
+                .build();
+        String returnValue;
+        try{
+            returnValue =  commandGateway.sendAndWait(productCommand);
+        }catch (Exception ex){
+            System.out.println("Error");
+            returnValue = ex.getLocalizedMessage();
+        }
+        return returnValue;
     }
 
     @GetMapping
     public String getProduct(){
-        return "Get";
+        return "Get"+this.env.getProperty("local.server.port");
     }
 
     @PutMapping
---
+++ products/src/main/java/com/mylearning/products/rest/core/events/ProductCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.mylearning.products.rest.core.events;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductCreatedEvent {
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+}
